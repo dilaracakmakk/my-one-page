be0aecbee931b4da5484649434d984f36fd9b5b6
dilaracakmakk <dilaraaacakmakk@gmail.com>
2024-01-20 22:48:55 +0300

    css

--- src/app/my-one-page-main/BlogCard.ts
+++ src/app/my-one-page-main/BlogCard.ts
@@ -7,13 +7,17 @@ export class blogCard{
     text:string;
     description:string;
     picture:string;
+    date:string;
+    link:string;
 
     //constructor
-    constructor(id:number,title:string, text:string, description:string,picture:string){
+    constructor(id:number,title:string, text:string, description:string,picture:string,date:string,link:string){
 this.id=id;
 this.title=title;
 this.text=text;
 this.description=description;
 this.picture=picture;
+this.date=date;
+this.link=link;
     }
 }

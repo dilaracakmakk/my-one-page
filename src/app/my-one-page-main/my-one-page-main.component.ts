43ad0dad79e95bc48261699bfa5dfdbb141e1801
dilaracakmakk <dilaraaacakmakk@gmail.com>
2024-01-21 19:58:18 +0300

    Update

--- src/app/my-one-page-main/my-one-page-main.component.ts
+++ src/app/my-one-page-main/my-one-page-main.component.ts
@@ -25,12 +25,10 @@ MyOnePageContent:MyOnePageContent[]=[
 
 
 blogCard:blogCard[]=[
-  {id:1, title:"Title-1", text:"Text-1", description:"Description-1", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
-  {id:2, title:"Title-2", text:"Text-2", description:"Description-2", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
-  {id:3, title:"Title-3", text:"Text-3", description:"Description-3", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
-  {id:4, title:"Title-4", text:"Text-4", description:"Description-4", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
-  {id:5, title:"Title-5", text:"Text-5", description:"Description-5", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
-  {id:6, title:"Title-6", text:"Text-6", description:"Description-6", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
+  {id:1, title:"Gaming", text:"Text-1", description:"Come and check out my gaming articles!", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
+  {id:2, title:"Music", text:"Text-2", description:"Those who trust their taste in music should come...", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
+  {id:3, title:"Photography", text:"Text-3", description:"There is so much worth seeing!", picture:"https://cdn.pixabay.com/photo/2016/01/09/18/27/camera-1130731_1280.jpg",date: String(new Date().getFullYear()),link:'https://deneme/',},
+  
 
 ];
 
